0305a4e07484e155f5b0089d28f3f718af1a5449
sam <sfriedman6711@gmail.com>
2024-05-23 23:54:39 -0600

    experimenting with paths, controlling scope, privacy

---
+++ my-project/restaurant/target/debug/deps/restaurant-ed851233e533bb94.d
@@ -0,0 +1,7 @@
+/home/sam/appas/resources/guessing_game/my-project/restaurant/target/debug/deps/librestaurant-ed851233e533bb94.rmeta: src/lib.rs
+
+/home/sam/appas/resources/guessing_game/my-project/restaurant/target/debug/deps/librestaurant-ed851233e533bb94.rlib: src/lib.rs
+
+/home/sam/appas/resources/guessing_game/my-project/restaurant/target/debug/deps/restaurant-ed851233e533bb94.d: src/lib.rs
+
+src/lib.rs:
---
+++ my-project/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/sam/appas/resources/guessing_game/my-project/restaurant/target/debug/librestaurant.rlib: /home/sam/appas/resources/guessing_game/my-project/restaurant/src/lib.rs
